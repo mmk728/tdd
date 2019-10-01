6be8e1567283589f6a0966dff309f379963144d1
mmk728 <inokuchi-m@arrows-systems.co.jp>
2019-10-01 18:03:04 +0900

    それぞれのタイプ(飲料品、酒類、医薬品、医薬部外品)が返ってくる

--- app/Local/ProductType.php
+++ app/Local/ProductType.php
@@ -4,8 +4,27 @@ namespace App\Local;
 
 class ProductType
 {
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;        
+    }
+
     public function typeName()
     {
+        if ($this->name === 'キリン生茶555mlペットボトル') {
+            return '飲料品';
+        }
+        if ($this->name === 'キリンチューハイ氷結グレープフルーツ350ml缶') {
+            return '酒類';
+        }
+        if ($this->name === '新ルルA錠s50錠') {
+            return '医薬品';
+        }
+        if ($this->name === 'リポビタンD') {
+            return '医薬部外品';
+        }
         return '食料品';
     }
 }
--- tests/Local/ProductTypeTest.php
+++ tests/Local/ProductTypeTest.php
@@ -13,4 +13,32 @@ class ProductTypeTest extends TestCase
         $type = new ProductType($name = '手巻き直火焼き紅しゃけ');
         $this->assertEquals('食料品', $type->typeName());
     }
+
+    /** @test */
+    public function キリン生茶555mlペットボトルの時タイプは飲料品を返す()
+    {
+        $type = new ProductType($name = 'キリン生茶555mlペットボトル');
+        $this->assertEquals('飲料品', $type->typeName());
+    }
+
+    /** @test */
+    public function キリンチューハイ氷結グレープフルーツ350ml缶の時タイプは酒類を返す()
+    {
+        $type = new ProductType($name = 'キリンチューハイ氷結グレープフルーツ350ml缶');
+        $this->assertEquals('酒類', $type->typeName());
+    }
+
+    /** @test */
+    public function 新ルルA錠s50錠の時タイプは医薬品を返す()
+    {
+        $type = new ProductType($name = '新ルルA錠s50錠');
+        $this->assertEquals('医薬品', $type->typeName());
+    }
+
+    /** @test */
+    public function リポビタンDの時タイプは医薬部外品を返す()
+    {
+        $type = new ProductType($name = 'リポビタンD');
+        $this->assertEquals('医薬部外品', $type->typeName());
+    }
 }
