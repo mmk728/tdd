6f5a86a288cc7dab525820d02004d55eed3ce4e7
mmk728 <inokuchi-m@arrows-systems.co.jp>
2019-10-02 12:25:56 +0900

    商品名にあった税抜き価格を取得できる

---
+++ tests/Models/FoodTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Models;
+
+use App\Models\Food;
+use Tests\TestCase;
+
+class FoodTest extends TestCase
+{
+    /** @test */
+    public function 手巻直火焼き紅しゃけの税抜き価格は139円である()
+    {
+        $this->assertEquals(139, (new Food($name = '手巻き直火焼き紅しゃけ'))->no_taxed_price);
+    }
+
+    /** @test */
+    public function 大きなおむすびマヨネーズの税抜き価格は186円である()
+    {
+        $this->assertEquals(186, (new Food($name = '大きなおむすびマヨネーズ'))->no_taxed_price);
+    }
+
+    /** @test */
+    public function 設定されていないキーにアクセスしようとするとエラーになる()
+    {
+        $this->expectException('\UnexpectedValueException');
+        (new Food($name = '手巻き直火焼き紅しゃけ'))->hoge;
+    }
+}
