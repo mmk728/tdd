d6b7d774743cb17d4ae82719e99fcca055090b61
mmk728 <inokuchi-m@arrows-systems.co.jp>
2019-10-03 11:26:27 +0900

    渡された商品の税込みの合計値の計算ができる

--- app/Caluclator.php
+++ app/Caluclator.php
@@ -5,11 +5,6 @@ namespace App;
 class Caluclator
 {
     private $products;
-    private $is_reduced_tax = [];
-    private $is_not_reduced_tax = [];
-
-    const REDUCED_TAXED_RATE = 8;
-    const NO_REDUCED_TAX_RATE = 10;
 
     public function __construct($products)
     {
@@ -18,37 +13,10 @@ class Caluclator
 
     public function total()
     {
+        $total_price = 0;
         foreach ($this->products as $product) {
-            if ($product->isReducedTaxRate()) {
-                if (array_key_exists($product->name, $this->is_reduced_tax) === false) {
-                    $this->is_reduced_tax += [$product->name => 0];
-                }
-                $this->is_reduced_tax[$product->name] += $product->no_taxed_price;
-                continue;
-            }
-            if (array_key_exists($product->name, $this->is_not_reduced_tax) === false) {
-                $this->is_not_reduced_tax += [$product->name => 0];
-            }
-            $this->is_not_reduced_tax[$product->name] += $product->no_taxed_price;
+            $total_price += $product->totalTaxedPrice();
         }
-        return intval($this->isReducedTaxTotal() + $this->isNotReducedTaxTotal());
-    }
-
-    private function isReducedTaxTotal()
-    {
-        $is_reduced_tax_total = 0;
-        foreach ($this->is_reduced_tax as $product_total) {
-            $is_reduced_tax_total += intval($product_total * (1 + self::REDUCED_TAXED_RATE / 100));
-        }
-        return $is_reduced_tax_total;
-    }
-
-    private function isNotReducedTaxTotal()
-    {
-        $is_not_reduced_tax_total = 0;
-        foreach ($this->is_not_reduced_tax as $product_total) {
-            $is_not_reduced_tax_total += intval($product_total * (1 + self::NO_REDUCED_TAX_RATE / 100));
-        }
-        return $is_not_reduced_tax_total;
+        return $total_price;
     }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -13,29 +13,32 @@ class CaluclatorTest extends TestCase
     /** @test */
     public function 手巻直火焼き紅しゃけ2個とキリンチューハイ氷結グレープフルーツ350ml缶3本を渡すと765が返ってくる()
     {
-        $products = [
-            new Food($name = '手巻き直火焼き紅しゃけ'),
-            new Food($name = '手巻き直火焼き紅しゃけ'),
-            new Liquor($name = 'キリンチューハイ氷結グレープフルーツ350ml缶'),
-            new Liquor($name = 'キリンチューハイ氷結グレープフルーツ350ml缶'),
-            new Liquor($name = 'キリンチューハイ氷結グレープフルーツ350ml缶'),
-        ];
-        $this->assertEquals(765, (new Caluclator($products))->total());
+        $food = new Food($name = '手巻き直火焼き紅しゃけ');
+        $food->add();
+        $food->add();
+        $liquor = new Liquor($name = 'キリンチューハイ氷結グレープフルーツ350ml缶');
+        $liquor->add();
+        $liquor->add();
+        $liquor->add();
+
+        $this->assertEquals(765, (new Caluclator($products = [$food, $liquor]))->total());
     }
 
     /** @test */
     public function 大きなおむすびマヨネーズ3個とキリン生茶555mlペットボトル4本と新ルルＡ錠s50錠1個を渡すと2164が返ってくる()
     {
-        $products = [
-            new Food($name = '大きなおむすびマヨネーズ'),
-            new Food($name = '大きなおむすびマヨネーズ'),
-            new Food($name = '大きなおむすびマヨネーズ'),
-            new Drink($name = 'キリン生茶555mlペットボトル'),
-            new Drink($name = 'キリン生茶555mlペットボトル'),
-            new Drink($name = 'キリン生茶555mlペットボトル'),
-            new Drink($name = 'キリン生茶555mlペットボトル'),
-            new Medicine($name = '新ルルA錠s50錠')
-        ];
-        $this->assertEquals(2164, (new Caluclator($products))->total());
+        $food = new Food($name = '大きなおむすびマヨネーズ');
+        $food->add();
+        $food->add();
+        $food->add();
+        $drink = new Drink($name = 'キリン生茶555mlペットボトル');
+        $drink->add();
+        $drink->add();
+        $drink->add();
+        $drink->add();
+        $medicine = new Medicine($name = '新ルルA錠s50錠');
+        $medicine->add();
+
+        $this->assertEquals(2164, (new Caluclator($products = [$food, $drink, $medicine]))->total());
     }
 }
